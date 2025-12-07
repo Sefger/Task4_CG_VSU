cad213bf0f75ed79bdf0e6ca5f89f2be4f321a32
Sefger <sefger2006@gmail.com>
2025-12-07 16:22:21 +0300

    add get/set for Vector 3f

--- src/main/java/com/cgvsu/math/Vector3f.java
+++ src/main/java/com/cgvsu/math/Vector3f.java
@@ -2,6 +2,9 @@ package com.cgvsu.math;
 
 // Это заготовка для собственной библиотеки для работы с линейной алгеброй
 public class Vector3f {
+    private static final float eps = 1e-7f;
+    public float x, y, z;
+
     public Vector3f(float x, float y, float z) {
         this.x = x;
         this.y = y;
@@ -9,10 +12,27 @@ public class Vector3f {
     }
 
     public boolean equals(Vector3f other) {
-        // todo: желательно, чтобы это была глобальная константа
-        final float eps = 1e-7f;
         return Math.abs(x - other.x) < eps && Math.abs(y - other.y) < eps && Math.abs(z - other.z) < eps;
     }
 
-    public float x, y, z;
+    public float getY() {
+        return this.y;
+    }
+
+    public float getX() {
+        return this.x;
+    }
+    public float getZ(){
+        return this.z;
+    }
+    public void setX(float x){
+        this.x = x;
+    }
+    public void setY(float y){
+        this.y = y;
+    }
+    public void setZ(float z){
+        this.z = z;
+    }
+
 }
