c63c2e1c2b52bf09892f16253ff361bfe41e3931
SallySallyS <sova.lizochka2006@gmail.com>
2025-12-09 16:35:54 +0300

    update Vector2f

--- src/main/java/com/cgvsu/math/Vector2f.java
+++ src/main/java/com/cgvsu/math/Vector2f.java
@@ -2,7 +2,7 @@ package com.cgvsu.math;
 
 // Это заготовка для собственной библиотеки для работы с линейной алгеброй
 public class Vector2f {
-    public float x, y;
+    final private float x, y;
 
     public Vector2f(float x, float y) {
         this.x = x;
