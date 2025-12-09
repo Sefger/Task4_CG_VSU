b0a2028dfd31129a7ccaa181b94a92cf92036e8d
SallySallyS <sova.lizochka2006@gmail.com>
2025-12-09 16:19:15 +0300

    update for ObjWriter

--- src/main/java/com/cgvsu/math/Vector2f.java
+++ src/main/java/com/cgvsu/math/Vector2f.java
@@ -2,10 +2,19 @@ package com.cgvsu.math;
 
 // Это заготовка для собственной библиотеки для работы с линейной алгеброй
 public class Vector2f {
+    public float x, y;
+
     public Vector2f(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
-    public float x, y;
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
 }
